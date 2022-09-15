2ed7935a9586be48d17b28802b85a306b7b41c9f
Cameron Thorp <109598902+Cameron-L-Thorp@users.noreply.github.com>
2022-09-14 19:31:13 -0600

    Add files via upload

---
+++ AHLCGConsole/AHLCGConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AHLCGConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
