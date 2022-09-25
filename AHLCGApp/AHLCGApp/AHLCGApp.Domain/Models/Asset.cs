b01fa7d57d466f7920611040f74e8b8037090f66
Cameron-L-Thorp <109598902+Cameron-L-Thorp@users.noreply.github.com>
2022-09-24 21:56:50 -0600

    started WinUI with EF

---
+++ AHLCGApp/AHLCGApp/AHLCGApp.Domain/Models/Asset.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHLCGApp.Domain.Models
+{
+    public class Asset
+    {
+        [Key] public int Id { get; set; }
+        public string Name { get; set; }
+        public string Subtype { get; set; }
+        public int Cost { get; set; }
+        public int Level { get; set; }
+        public int ClassType { get; set; }
+        public bool IsMultiClass { get; set; }
+        public string TraitOne { get; set; }
+        public string? TraitTwo { get; set; }
+        public string? TraitThree { get; set; }
+        public string AbilityOne { get; set; }
+        public string? AbilityTwo { get; set; }
+        public string? AbilityThree { get; set; }
+        //public List<Trait> Traits { get; set; }
+        //public List<Ability> Abilities { get; set; }
+        public int Health { get; set; }
+        public int Sanity { get; set; }
+        public string Slot { get; set; }
+        public int WillVal { get; set; }
+        public int IntVal { get; set; }
+        public int CombatVal { get; set; }
+        public int AgilityVal { get; set; }
+        public int CampaignId { get; set; }
+    }
+}
---
+++ AHLCGApp/AHLCGApp/AHLCGApp.Domain/Models/Investigator.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace AHLCGApp.Domain.Models
+{
+    public class Investigator
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ClassType { get; set; }
+        public int SecondaryClassType { get; set; }
+        public string FlavorText { get; set; }
+        public int MaxHealth { get; set; }
+        public int MaxSanity { get; set; }
+        public int Willpower { get; set; }
+        public int Intellect { get; set; }
+        public int Combat { get; set; }
+        public int Agility { get; set; }
+        public string TraitOne { get; set; }
+        public string? TraitTwo { get; set; }
+        public string? TraitThree { get; set; }
+        public string AbilityOne { get; set; }
+        public string? AbilityTwo { get; set; }
+        public string? AbilityThree { get; set; }
+        //public List<Trait> Traits { get; set; }
+        //public List<Ability> Abilities { get; set; }
+        public string ElderSignAbility { get; set; }
+        public int DeckSize { get; set; }
+    }
+
+    //public class Trait
+    //{
+    //    public int Id { get; set; }
+    //    public string TraitName { get; set; }
+    //}
+    //public class Ability
+    //{
+    //    public int Id { get; set; }
+    //    public string AbilityName { get; set; }
+    //}
+}
