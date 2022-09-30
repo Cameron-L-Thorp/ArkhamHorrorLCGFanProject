07d5524ffe202b203d8b186aa68653ead8f34f11
Cameron-L-Thorp <109598902+Cameron-L-Thorp@users.noreply.github.com>
2022-09-29 20:13:00 -0600

    spinning wheels with clearing data

--- AHLCGAppWinUI/AHLCGApp/AHLCGApp/AHLCGApp/InvestigatorPage.xaml.cs
+++ AHLCGAppWinUI/AHLCGApp/AHLCGApp/AHLCGApp/InvestigatorPage.xaml.cs
@@ -11,9 +11,11 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Mime;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using ABI.Windows.UI.Popups;
 using AHLCGApp.EF.CRUD;
 using AHLCGApp.Domain.Models;
 using AHLCGApp.Domain;
@@ -36,51 +38,70 @@ namespace AHLCGApp
 
         public void AddInv_OnClick(object sender, RoutedEventArgs e)
         {
-            string pClass = PClass.SelectedValue.ToString();
-            string sClass = SClass.SelectedValue.ToString();
-            Investigator inv = new Investigator();
-            inv.Name = InvName.Text;
-            inv.Subtitle = SubtitleName.Text;
-            inv.ClassType = pClass;
-            inv.SecondaryClassType = sClass;
-            inv.MaxHealth = Convert.ToInt32(HealthValue.Text);
-            inv.MaxSanity = Convert.ToInt32(SanityValue.Value);
-            inv.Willpower = Convert.ToInt32(WillpowerValue.Text);
-            inv.Intellect = Convert.ToInt32(IntellectValue.Value);
-            inv.Combat = Convert.ToInt32(CombatValue.Value);
-            inv.Agility = Convert.ToInt32(AgilityValue.Value);
-            inv.TraitOne = FTrait.Text;
-            inv.AbilityOne = FAbility.Text;
-            inv.ElderSignAbility = ESAbility.Text;
-            inv.DeckSize = 30;
-            if (Flavor.Text != null)
+            try
             {
-                inv.FlavorText = Flavor.Text;
+                string pClass = PClass.SelectedValue.ToString();
+                string sClass = SClass.SelectedValue.ToString();
+                Investigator inv = new Investigator();
+                inv.Name = InvName.Text;
+                inv.Subtitle = SubtitleName.Text;
+                inv.ClassType = pClass;
+                inv.SecondaryClassType = sClass;
+                inv.MaxHealth = Convert.ToInt32(HealthValue.Text);
+                inv.MaxSanity = Convert.ToInt32(SanityValue.Value);
+                inv.Willpower = Convert.ToInt32(WillpowerValue.Text);
+                inv.Intellect = Convert.ToInt32(IntellectValue.Value);
+                inv.Combat = Convert.ToInt32(CombatValue.Value);
+                inv.Agility = Convert.ToInt32(AgilityValue.Value);
+                inv.TraitOne = FTrait.Text;
+                inv.AbilityOne = FAbility.Text;
+                inv.ElderSignAbility = ESAbility.Text;
+                inv.DeckSize = 30;
+                if (Flavor.Text != null)
+                {
+                    inv.FlavorText = Flavor.Text;
+                }
+                if (STrait.Text != null)
+                {
+                    inv.TraitTwo = STrait.Text;
+                }
+                if (TdTrait.Text != null)
+                {
+                    inv.TraitThree = TdTrait.Text;
+                }
+                if (SAbility.Text != null)
+                {
+                    inv.AbilityTwo = SAbility.Text;
+                }
+                if (TdAbility.Text != null)
+                {
+                    inv.AbilityThree = TdAbility.Text;
+                }
+                InvestigatorRepository invRepo = new InvestigatorRepository();
+                InvestigatorRepository.AddOrUpdateInvestigator(inv);
+                foreach (UIElement child in InvestigatorGrid.Children)
+                {
+                    child.ClearValue(null);
+                }
+                
+                
             }
-            if (STrait.Text != null)
+            catch (Exception ex)
             {
-                inv.TraitTwo = STrait.Text;
+                
             }
-            if (TdTrait.Text != null)
-            {
-                inv.TraitThree = TdTrait.Text;
-            }
-            if (SAbility.Text != null)
-            {
-                inv.AbilityTwo = SAbility.Text;
-            }
-            if (TdAbility.Text != null)
-            {
-                inv.AbilityThree = TdAbility.Text;
-            }
-            InvestigatorRepository invRepo = new InvestigatorRepository();
-            InvestigatorRepository.AddOrUpdateInvestigator(inv);
-            this.InitializeComponent();
+            
         }
 
         private void DontAddInv_OnClick(object sender, RoutedEventArgs e)
         {
+            
+        }
 
+
+        private void Asset_Checked(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
