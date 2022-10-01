cf9cedad52a15df8db7804443ee1678a664bf53a
Cameron-L-Thorp <109598902+Cameron-L-Thorp@users.noreply.github.com>
2022-10-01 15:43:49 -0600

    new framework for proper UI navigation

---
+++ AHLCGAppWinUI/ArkhamHorrorUI/ArkhamHorrorUI/AHShell.xaml.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace ArkhamHorrorUI
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+        
+        private void ArkhamHorrorNav_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void NavigationView_SelectionChanged(NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            
+        }
+    }
+}
